ca5a97ed3869d5e9cf5ef0b07e2400fb9866022e
victor ukwuezeh <victorukwuezeh@gmail.com>
2022-04-10 20:07:19 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-const apiUrl = "https://restcountries.eu/rest/v2/name/";
+const apiUrl = "https://restcountries.com/v2/name/";
 const countryInput = document.querySelector('#input');
+const SearchBtn = document.querySelector('#searchBtn');
 const row = document.querySelector('.row');
 
 
-countryInput.addEventListener('keyup',(e)=>{
-  let country = e.target.value;
-  if(country.length != 0 && length>3){
+SearchBtn.addEventListener('click',()=>{
+  let country = countryInput.value;
+
+ setTimeout(function(){
+  console.log(country);
+ },5000)
+
+  if(country.length != 0){
       let apidata = `${apiUrl}${country}`;
   row.innerHTML = "";
   fetch(apidata)
@@ -13,9 +19,9 @@ countryInput.addEventListener('keyup',(e)=>{
     .then((data) => {
       splice(data);
     })
-    .catch((err) => err);
+    .catch((err) => console.log(err));
   }else{
-  row.innerHTML = "";
+  row.innerHTML = "search field is required";
 
   }
 
@@ -23,8 +29,9 @@ countryInput.addEventListener('keyup',(e)=>{
 
 
   const splice = (data) => {
-          let count = 10;
-      for (let x = 0; x <= count; x++) {
+    console.log(data);
+        
+      for (let x = 0; x <= data.length; x++) {
         process(data[x]);
       }
      
@@ -33,39 +40,49 @@ countryInput.addEventListener('keyup',(e)=>{
 
 
   const process = (data) => {
-    let htmlData = `        <div class="card">
-                        <div class="flag"> <img src="${(data.flag != "undefined")?  data.flag : ""}" alt="${data.name}" > </div>
+      if(data.area ==undefined){
+        data.area = ""
+      }
+      if(data.borders ==undefined){
+        data.borders = ""
+      }
+      if(data.regionalBlocs == undefined){
+        data.regionalBlocs = ""
+      }
+    let htmlData = `<div class="card">
+                        <div class="flag" style="background-image:url('${(data.flag != "undefined")?  data.flag : ""}" alt="${data.name}');">  </div>
                         <div class="details">
                             <div class="name">Country name: ${data.name} </div>
                             <div class="nativename">Native name:${data.nativeName}</div>
-                            <div class="subrigion">Sub-rigion:${data.subregion}</div>
-                            <div class="region">Rigion:${data.region}</div>
-                            <div class="capital">Capital:${data.capital}</div>
-                            <div class="calling-code">Calling Code:+${data.callingCodes}</div>
-                            <div class="population">Population:${data.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</div>
-                            <div class="language">Language:${data.languages[0].name}</div>
-                            <div class="language">Native language:${data.languages[0].nativeName}</div>
-                            <div class="timezone">Timezone:${data.timezones}</div>
-                            <div class="topleveldomain">Domain:${data.topLevelDomain}</div>
-                            <div class="lontitiude">longtitude & Latitude:${data.latlng}</div>
-                            <div class="area">Area:${data.area.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }m<sup>2</sup></div>
-                            <div class="gini">Gini index:${data.gini}</div>
-                            <div class="border">Bordering countries:${JSON.stringify(data.borders).replace(/[#_"]]/g, "")}</div>
-                            <div class="numeric-code">Numeric code:${data.numericCode}</div>
-                            <div class="currency-name">Currency name:${data.currencies[0].name}</div>
-                            <div class="currency-code">Currency code:${data.currencies[0].code}</div> 
-                            <div class="currency-symbol">Currency symbol:${data.currencies[0].symbol }</div>
-                            <div class="language">Language:${data.languages[0].name}</div>
-                            <div class="navtive-language">Native Langaue:${data.languages[0].nativeName }</div>
-                            <div class="language-abbr">Language abbr:${data.languages[0].iso639_2}</div>
-                            <div class="regional-Blocs-name">Rigional Blocs:${(data.regionalBlocs.length !=0 )? data.regionalBlocs[0].name : "" }</div>
-                            <div class="regional-Blocs-abrr">Regional Blocs Abrr:${(data.regionalBlocs.length != 0)? data.regionalBlocs[0].acronym : ""}</div>
+                            <div class="subrigion">Sub-rigion: ${data.subregion}</div>
+                            <div class="region">Rigion: ${data.region}</div>
+                            <div class="capital">Capital: ${data.capital}</div>
+                            <div class="calling-code">Calling Code: +${data.callingCodes}</div>
+                            <div class="population">Population: ${data.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</div>
+                            <div class="language">Language: ${data.languages[0].name}</div>
+                            <div class="language">Native language: ${data.languages[0].nativeName}</div>
+                            <div class="timezone">Timezone: ${data.timezones}</div>
+                            <div class="topleveldomain">Domain: ${data.topLevelDomain}</div>
+                            <div class="lontitiude">longtitude & Latitude: ${data.latlng}</div>
+                            <div class="area">Area: ${data.area.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }m<sup>2</sup></div>
+                            <div class="gini">Gini index: ${data.gini}</div>
+                            <div class="border">Bordering countries: ${JSON.stringify(data.borders).replace(/[#_"]]/g, "")}</div>
+                            <div class="numeric-code">Numeric code: ${data.numericCode}</div>
+                            <div class="currency-name">Currency name: ${data.currencies[0].name}</div>
+                            <div class="currency-code">Currency code: ${data.currencies[0].code}</div> 
+                            <div class="currency-symbol">Currency symbol: ${data.currencies[0].symbol }</div>
+                            <div class="language">Language: ${data.languages[0].name}</div>
+                            <div class="navtive-language">Native Langaue: ${data.languages[0].nativeName }</div>
+                            <div class="language-abbr">Language abbr: ${data.languages[0].iso639_2}</div>
+                            <div class="regional-Blocs-name">Rigional Blocs: ${(data.regionalBlocs.length !=0 )? data.regionalBlocs[0].name : "" }</div>
+                            <div class="regional-Blocs-abrr">Regional Blocs Abrr: ${(data.regionalBlocs.length != 0)? data.regionalBlocs[0].acronym : ""}</div>
                         </div>
 
                     </div>`;
 
    row.innerHTML += htmlData;
   };
+
 
 
 
